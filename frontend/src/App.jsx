8a09fff4a5f3ac39f232728435582254bd79efc8
Yasas Banuka <yixbinfo@gmail.com>
2025-11-22 21:44:45 +0530

    feat(app): add sky images to background and made the base structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-  return <></>;
+  return (
+    <div className="bg-[url('/2.jpg')] bg-cover bg-center h-dvh text-white">
+      <div className="bg-black/10 inset-0 absolute"></div>
+      <div className="flex flex-column gap-4 py-6 px-5">
+        <div className="left-section glassmorphism text-center flex flex-column">
+          <h1 className="font-logo font-bold text-2xl">SKYLITHIC</h1>
+        </div>
+        <div className="right-section"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
