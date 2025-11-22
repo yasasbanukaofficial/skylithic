062c4e4858cac2b9cd71443082a6ae5c032698dd
Yasas Banuka <yixbinfo@gmail.com>
2025-11-22 22:58:48 +0530

    feat(shadcn): added shadcn and recharts for chart display

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      // Map the '@/...' alias to the actual path './src'
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
