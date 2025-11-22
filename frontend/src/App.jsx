60fe28f07c0c59fbbdc06ea494d62e68fd275821
Yasas Banuka <yixbinfo@gmail.com>
2025-11-23 00:56:10 +0530

    feat(app): implemented geo header and made sections responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { ChartLineDefault } from "./components/ChartDisplay";
+import { ChartLineDefault } from "./components/layout/ChartDisplay";
+import { GeoHeader } from "./components/layout/GeoHeader";
 
 function App() {
   return (
     <div className="bg-[url('/sunny-cloudy.jpg')] bg-cover bg-center h-dvh text-white flex flex-col">
       <div className="bg-black/10 inset-0 absolute z-0"></div>
 
-      <div className="flex flex-row gap-4 py-13 px-15">
-        <div className="left-section glassmorphism text-center w-75 flex flex-col gap-6">
+      <div className="flex flex-col-reverse sm:flex-col lg:flex-row gap-12 py-13 px-15">
+        <div className="left-section glassmorphism text-center w-75 flex flex-col gap-6 rounded-4xl">
           <h1 className="font-medium text-2xl font-logo underline underline-offset-8">
             SkyLithic
           </h1>
@@ -16,7 +17,9 @@ function App() {
           </div>
         </div>
 
-        <div className="flex-2/4 right-section"></div>
+        <div className="flex-2/4 right-section py-6">
+          <GeoHeader />
+        </div>
       </div>
     </div>
   );
