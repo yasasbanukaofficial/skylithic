16e38326243591117bfdac970cf859596e8a69cf
Yasas Banuka <yixbinfo@gmail.com>
2025-11-24 16:17:50 +0530

    feat(backend): allow cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import weatherRoutes from "./routes/weather.routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/weather", weatherRoutes);
 
